2300c48586c8478a865f1edbf23d509960773905
ivanmd96 <ivan96dantas@gmail.com>
2025-03-24 18:14:22 -0300

    subindo ajustes 2

---
+++ terraform/main.tf
@@ -0,0 +1,53 @@
+provider "google" {
+  credentials = file("terraform-key.json") # ou o caminho correto na sua máquina
+  project     = var.project_id
+  region      = var.region
+}
+
+resource "google_compute_instance" "vm" {
+  name         = var.vm_name
+  machine_type = var.machine_type
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = var.vm_image
+    }
+  }
+
+  network_interface {
+    network    = google_compute_network.vpc_network.id
+    subnetwork = google_compute_subnetwork.subnet.id
+    access_config {}
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file(var.ssh_key_path)}"
+  }
+}
+
+
+resource "google_compute_network" "vpc_network" {
+  name                    = var.vpc_name
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "subnet" {
+  name          = var.subnet_name
+  region        = var.region
+  network       = google_compute_network.vpc_network.id
+  ip_cidr_range = var.subnet_range
+}
+
+resource "google_compute_firewall" "allow_ssh" {
+  name    = "allow-ssh"
+  network = google_compute_network.vpc_network.id
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
