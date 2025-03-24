fab45c84a803bae5734a3cd24d77bfb72cb7db1d
ivanmd96 <ivan96dantas@gmail.com>
2025-03-24 19:42:03 -0300

    ajustando main

--- terraform/main.tf
+++ terraform/main.tf
@@ -29,7 +29,6 @@ resource "google_compute_instance" "vm" {
 
 data "google_compute_network" "vpc_network" {
   name                    = var.vpc_name
-  auto_create_subnetworks = false
 }
 
 data "google_compute_subnetwork" "subnet" {
