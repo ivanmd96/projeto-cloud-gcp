af5660eab56bb5e7b4e576fa594f839ea45574fe
ivanmd96 <ivan96dantas@gmail.com>
2025-03-26 18:48:08 -0300

    ajuste no terraform 7

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,7 +9,7 @@ module "vm" {
   vm_name          = var.vm_name
   machine_type     = var.machine_type
   vm_image         = var.vm_image
-  ssh_key_path     = var.ssh_key_path
+  sh_public_key     = var.sh_public_key
   zone             = var.zone
   subnetwork_id    = module.network.subnet_id  # ← valor vindo do output do módulo network
   network_id       = module.network.vpc_id      # ← valor vindo do output do módulo network
