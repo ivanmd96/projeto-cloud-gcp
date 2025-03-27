e13618f8403a35ddc9058bf6b2334f1c6a29582b
ivanmd96 <ivan96dantas@gmail.com>
2025-03-27 19:00:45 -0300

    ajuste no terraform 20

--- terraform/modules/network/outputs.tf
+++ terraform/modules/network/outputs.tf
@@ -1,7 +1,9 @@
 output "vpc_id" {
-  value = google_compute_network.vpc_network.id
+  description = "ID da VPC criada"
+  value       = data.google_compute_network.vpc_network.id
 }
 
 output "subnet_id" {
-  value = google_compute_subnetwork.subnet.id
+  description = "ID da Subrede criada"
+  value       = data.google_compute_subnetwork.subnet.id
 }
