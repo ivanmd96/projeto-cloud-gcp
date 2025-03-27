3c13821af6df005a95759ea0a514982e8385d3fa
ivanmd96 <ivan96dantas@gmail.com>
2025-03-27 18:48:31 -0300

    ajuste no terraform 15

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,7 +23,6 @@ data "google_compute_network" "vpc_network" {
 # Criação da sub-rede baseada no ambiente
 data "google_compute_subnetwork" "subnet" {
   name          = var.subnet_name
-  ip_cidr_range = var.subnet_range
   region        = var.region
   network       = data.google_compute_network.vpc_network.id
 }
