987eeb894fc73efe4da54800f38ce0d54f177f20
ivanmd96 <ivan96dantas@gmail.com>
2025-03-27 18:57:17 -0300

    ajuste no terraform 18

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -1,6 +1,6 @@
 resource "google_compute_subnetwork" "subnet" {
   name          = var.subnet_name
   ip_cidr_range = var.subnet_range
-  network       = data.google_compute_network.vpc_network.id
+  network       = var.network_id
   region        = var.region
 }
