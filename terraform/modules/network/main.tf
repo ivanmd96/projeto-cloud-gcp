3e8f9e224a708c94e874d769b6801f478e5a1be2
ivanmd96 <ivan96dantas@gmail.com>
2025-03-31 21:29:23 -0300

    subindo backend e criando bucket para backend do terraform 41

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -3,13 +3,4 @@ data "google_compute_subnetwork" "subnet" {
   ip_cidr_range = var.subnet_range
   region        = var.region
   network       = var.vpc_self_link
-
-  # Melhor prática para evitar destruição acidental
-  lifecycle {
-    prevent_destroy = true
-    ignore_changes = [
-      # Evita recriação se algo irrelevante mudar
-      secondary_ip_range
-    ]
-  }
 }
